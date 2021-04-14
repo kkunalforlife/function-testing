d18a15f5e9aa0e7d1e20461fd2d0a583716dcc8f
kunalkalra <kkalra@petsafe.net>
2021-04-14 12:43:08 +0100

    changes

---
+++ test.js
@@ -0,0 +1,17 @@
+let no1 = 50
+
+
+function addnumber(no1 , no2){
+let no3 =  no1 +  no2;
+  console.log(no3);
+}
+
+addnumber(2, 3)
+conversec(no1)
+
+
+function conversec(no1){
+    let no2 = no1*60 ;
+    console.log( "the no of sec are" + "  for the mins "+ no1 + " are"+  no2);
+    return no2;
+}
